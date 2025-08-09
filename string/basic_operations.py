4dcfd75e9e65465d52347c9ea3819971ecb70330
bilel.majdoub <bilel.majdoub@epita.fr>
2025-08-09 17:14:19 +0200

    feat: get_length

--- string/basic_operations.py
+++ string/basic_operations.py
@@ -6,8 +6,20 @@ def length(s):
 
     return len(s)
 
+def get_length(s):
+    """renvoie la longueur de la chaine s"""
+
+    cpt = 0
+
+    for k in s:
+        cpt = cpt + 1
+
+    return cpt
 
 
 if __name__ == '__main__':
     assert length("Epita") == 5
     assert length(" ") == 1
+
+    assert get_length("Epita") == 5
+    assert get_length("") == 0
