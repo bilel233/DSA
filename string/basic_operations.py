c732a3fdc08d3ced15e5bf6ccf859f297d1c714d
bilel.majdoub <bilel.majdoub@epita.fr>
2025-08-09 17:09:06 +0200

    feat: length

---
+++ string/basic_operations.py
@@ -0,0 +1,13 @@
+#dans cet exercice, nous allons revoir les exercices sur les strings
+
+
+def length(s):
+    """renvoie la longueur de la chaines de caracteres s"""
+
+    return len(s)
+
+
+
+if __name__ == '__main__':
+    assert length("Epita") == 5
+    assert length(" ") == 1
